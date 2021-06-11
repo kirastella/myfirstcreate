48775e947e2f60aa62d91830d0f144bc7980c3bd
kirastella <72187326+kirastella@users.noreply.github.com>
2021-06-11 13:32:11 +0200

    icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
-import React from "react";
+import Icon from './Icons'
 import './header.scss'
 
 function Header(){
     return (
         <header className="header">
-            <h2 className="header__headline">Good morning Akila!</h2>
-            <figure></figure>
+            <h2 className="header__headline">Good morning Akila!</h2><Icon />
         </header>
     )
 }
---
+++ src/components/Icons.js
@@ -0,0 +1,7 @@
+import {FaShoppingCart} from 'react-icons/fa'
+
+const Icons  = () => {
+    return <FaShoppingCart />;
+}
+ 
+export default Icons; 
