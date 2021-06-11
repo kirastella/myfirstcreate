f0313b667d81c8686c9679e5c60e8f8150513174
kirastella <72187326+kirastella@users.noreply.github.com>
2021-06-11 13:23:01 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './header.scss'
+
+function Header(){
+    return (
+        <header className="header">
+            <h2 className="header__headline">Good morning Akila!</h2>
+            <figure></figure>
+        </header>
+    )
+}
+export default Header;
